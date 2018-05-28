432809a263a5b9aaecb0015cf1b1f7b90715d516
Tsunemasa Hachiya <eighty8.me@gmail.com>
2018-05-29 00:52:03 +0900

    Modify get chat message
    チャットメッセージの取得方法を変更

--- FirestoreExample/ViewController.swift
+++ FirestoreExample/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController {
     
     let myId = "vq7ogXCvOeQsNC7qbJST"
     let yourId = "aMBVU5lPemaoAbjBmcV5"
-    var roomId = ""
+    var roomCount = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,122 +34,66 @@ class ViewController: UIViewController {
     }
 
     @IBAction func didTappedDataCreateButton(_ sender: UIButton) {
-//        guard let data = dataCreateTextField.text else { return }
-        
-        let chatsRef = db.collection("Chats").document(roomId).collection("messages")
-        chatsRef.getDocuments { (snapshot, error) in
-            if let err = error {
-                print("Error writing document: \(err)")
-            } else {
-                self.createMessage(dataCount: snapshot?.count)
-            }
-        }
-        
-    }
-    
-    private func createMessage(dataCount: Int?) {
-        
-        let count = dataCount ?? 0
-        
-        let myMessage = [
-            "uid": myId,
-            "name": "tsune",
-            "gender": "male",
-            "age": 39,
-            "message": "kyonさんこんにちわ！",
-            "lastUpdated": FieldValue.serverTimestamp()
-        ] as [String: Any]
-        
-        let yourMessage = [
-            "uid": yourId,
-            "name": "hono",
-            "gender": "female",
-            "age": 32,
-            "message": "朝霞なんですね^^ わたしは大宮ですよー！",
-            "lastUpdated": FieldValue.serverTimestamp()
-        ] as [String: Any]
-        
-        let messageId = "\(count + 1)"
-        
-        db.collection("Chats").document(roomId)
-            .collection("messages").document(messageId).setData(myMessage) { error in
-                if let err = error {
-                    print("Error writing document: \(err)")
-                } else {
-                    print("Document successfully written!")
-                }
-        }
-    }
-    
-    @IBAction func didTappedDataUpdateButton(_ sender: UIButton) {
-//        guard let data = dataUpdateTextField.text else { return }
     }
     
     @IBAction func didTappedDataSearchButton(_ sender: UIButton) {
-        
-        roomId = "Room_\(yourId)"
-        
-        db.collection("Chats").document(roomId).collection("Messages")
-            .addSnapshotListener { documentSnapshot, error in
-                guard let document = documentSnapshot else {
-                    print("Error fetching document: \(error!)")
-                    return
-                }
-                let source = document.metadata.hasPendingWrites ? "Local" : "Server"
-                print("\(source): ", document.documents.count)
-                let docs = document.documents
-                docs.forEach {
-                    print("\(source):", $0.data())
-                }
+
+        var messages = [[String: Any]]()
+        let chatsRef = db.collection("Chats")
+        let query1 = chatsRef.whereField("senderId", isEqualTo: myId).whereField("receiverId", isEqualTo: yourId)
+        let query2 = chatsRef.whereField("senderId", isEqualTo: yourId).whereField("receiverId", isEqualTo: myId)
+        self.getMessage(query: query1, callback: { data1 in
+
+            self.getMessage(query: query2, callback: { data2 in
+                messages = data1 + data2
+//                print(messages)
+                messages = messages.sorted(by: { (data1, data2) -> Bool in
+                    guard let createdAt1 = data1["createdAt"] else { return false }
+                    guard let createdAt2 = data2["createdAt"] else { return false }
+
+                    let date1 = createdAt1 as! Date
+                    let date2 = createdAt2 as! Date
+                    
+                    if date1 < date2 {
+                        return true
+                    } else {
+                        return false
+                    }
+                })
+//                print(messages)
                 
-//                let _ = self.db.collection("Chats").document(self.roomId)
-//                    .collection("messages").whereField("name", isEqualTo: "tsune")
-//                    .getDocuments(completion: { (querySnapshot, error) in
-//                        if let err = error {
-//                            print("Error fetching document: \(err)")
-//                        } else {
-//                            for document in querySnapshot!.documents {
-//                                print(document.data())
-//                            }
-//                        }
-//                    })
+                self.display(messages: messages)
+            })
+        })
+
+    }
+    
+    private func getMessage(query: Query, callback: @escaping (_ data: [[String: Any]]) -> Void) {
+        var chatData = [[String: Any]]()
+        query.addSnapshotListener { querySnapshot, error in
+            if let err = error {
+                print("Error fetching document: \(err)")
+                return
+            }
+            guard let document = querySnapshot else { return }
+            let docs = document.documents
+            docs.forEach {
+                chatData.append($0.data())
+            }
+            callback(chatData)
+        }
+    }
+    
+    private func display(messages: [[String: Any]]) {
+        print("--- Display ---")
+        for message in messages {
+            print(message)
         }
     }
     
     @IBAction func didTappedListSearchButton(_ sender: UIButton) {
-        db.collection("Users").document(myId).collection("Rooms").getDocuments() { querySnapshot, error in
-            if let err = error {
-                print("Error getting documents: \(err)")
-            } else {
-                for document in querySnapshot!.documents {
-                    // Collection Reference Type から 参照先のDocumentIDを取得
-                    let docRef = document.data()
-                    let chatRoom = docRef["Romm"] as? DocumentReference
-                    
-                    print(chatRoom?.documentID ?? "")
-                    
-                    self.db.collection("Chats").document((chatRoom?.documentID)!).collection("Messages")
-                        .addSnapshotListener { (snapShot, error) in
-                        
-                        guard let document = snapShot else {
-                            print("Error fetching document: \(error!)")
-                            return
-                        }
-                        let source = document.metadata.hasPendingWrites ? "Local" : "Server"
-                        print("\(source): ", document.documents.count)
-                        let docs = document.documents
-                        docs.forEach {
-                            print("\(source):", $0.data())
-                        }
-                    }
-                }
-            }
-        }
+        
     }
-    
-    @IBAction func didTappedDataDeleteButton(_ sender: UIButton) {
-//        guard let data = dataDeleteTextField.text else { return }
-    }
-    
+
 }
 
